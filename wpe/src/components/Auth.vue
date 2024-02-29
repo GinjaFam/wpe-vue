a0cc4aa60cf24bebf5443731cb1b427f5d19f8f3
GinjaFam <GinjaFam@users.noreply.github.com>
2024-02-29 22:03:17 +0000

    auth component working

--- wpe/src/components/Auth.vue
+++ wpe/src/components/Auth.vue
@@ -4,13 +4,15 @@
         <Registration ref="registrationComponent" />
         <button type="button" @click="openRegistrationModal" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signupModal">Register</button>
         <Login ref="loginComponent" />
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</button>
+        <button type="button" @click="logOut" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</button>
     </div>
 </template>
 
 <script>
 import Registration from './Register.vue';
 import Login from './Login.vue';
+import { userAuthStore } from '@/stores/auth';
+
 
 export default {
     name: 'Auth',
@@ -24,6 +26,33 @@ export default {
         },
         openLoginModal() {
             this.$refs.loginComponent.showModal = true;
+        },
+        logOut () {
+            fetch('/api/logout', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            })
+            .then(response => { 
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw response.json();
+                }
+            })
+            .then(data => {
+                console.log(data);
+                // set global variable to null
+                const authStore = userAuthStore();
+                authStore.logUserOut();
+                // redirect to home
+                this.$router.push({ path: '/' });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
         }
 
     }
